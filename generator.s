bdbdf7b97811a15ef9f858ee341c14392221598b
CiaccoDavide <ciaccodavide@gmail.com>
2015-06-10 23:25:59 +0200

    introdotto l'uso della stack

--- generator.s
+++ generator.s
@@ -2,6 +2,7 @@
 #	$s1 seed
 #	$s2 string pointer
 #	$s3 string pointer (reset default)
+#	$s4 string pointer dinamico
 
 
 
@@ -20,6 +21,8 @@ string1: .asciiz "\nInserire il seed: "
 main:
 
 la $s2, labirinto 	#non serve caricare subito la default finchè non si vuole fare il reset
+					#$s2 invece mi serve per calcolare l'offset sulla stringa $s4 > $s2+11
+la $s4, labirinto 	
 
 jal seed
 
@@ -37,98 +40,17 @@ addi $t2, $t2, 11	#11+((2*x)+(20*y))
 
 #POSIZIONO IL PUNTO DI PARTENZA
 li $t0, 65			#carico il carattere A di partenza in $t0
-add $s2, $s2, $t2	#sposto il puntatore sulla casella
-sb $t0, ($s2)		#salva il char nel buffer(?)
+add $s4, $s4, $t2	#sposto il puntatore sulla casella
+sb $t0, ($s4)		#salva il char nel buffer(?)
 
 
 
 
+jal passo
 
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-passo:
-
-
-
-li $v0, 4				# selezione di print_string
-la $a0, labirinto		# $a0 = indirizzo di string2
-syscall					# lancio print_string
-
-
-
-
-jal rand 			#GENERA la direzione
-#$t2 contiene la direzione
-#$s2 contiene il pointer
-beq $t2, $zero, nord 	#0:nord
-addi $t2, $t2, -1
-beq $t2, $zero, est 	#1:est
-addi $t2, $t2, -1
-beq $t2, $zero, sud 	#2:sud
-addi $t2, $t2, -1
-beq $t2, $zero, ovest 	#3:ovest
-j ExitSwitch
-
-nord:
-blt $s2, 20, ExitSwitch	#controlla se può andare a nord
-addi $s2, $s2, -10
-jal dot
-addi $s2, $s2, -10
-jal dot
-j ExitSwitch
-
-est:
-beq $s2, 17, ExitSwitch	#controlla se può andare a est
-beq $s2, 37, ExitSwitch
-beq $s2, 57, ExitSwitch
-beq $s2, 87, ExitSwitch
-addi $s2, $s2, -1
-jal dot
-addi $s2, $s2, -1
-jal dot
-j ExitSwitch
-
-sud:
-bgt $s2, 60, ExitSwitch	#controlla se può andare a sud
-addi $s2, $s2, -10
-jal dot
-addi $s2, $s2, -10
-jal dot
-j ExitSwitch
-
-ovest:
-beq $s2, 11, ExitSwitch	#controlla se può andare a ovest
-beq $s2, 31, ExitSwitch
-beq $s2, 51, ExitSwitch
-beq $s2, 81, ExitSwitch
-addi $s2, $s2, -1
-jal dot
-addi $s2, $s2, -1
-jal dot
-j ExitSwitch
-
-ExitSwitch:
 
 
 
@@ -165,18 +87,18 @@ syscall
 
 
 pre_reset:
-la $s1, labirinto
+la $s2, labirinto
 la $s3, default
 
 reset: #reset funzionante!
 
 lb $t0, ($s3)	#prende un char dalla stringa di default
-sb $t0, ($s1)	#salva il char nel buffer(?)
+sb $t0, ($s2)	#salva il char nel buffer(?)
 
-beq $t0, 0 main
+beq $t0, 0 main	#exit loop
 
 addi $s3, $s3, 1 	#incremento i puntatori
-addi $s1, $s1, 1
+addi $s2, $s2, 1
 
 j reset 		#loop
 
@@ -221,7 +143,87 @@ jr $ra
 
 dot:
 li $t0, 46			#carico il carattere . in $t0
-sb $t0, ($s2)		#salva il char
+sb $t0, ($s4)		#salva il char
 jr $ra
 
 
+
+passo:
+#stack save
+addi $sp, $sp, -4
+sw $ra, 0($sp)
+
+
+#PROVVISORIO stampa il labirinto
+li $v0, 4				# selezione di print_string
+la $a0, labirinto		# $a0 = indirizzo di string2
+syscall					# lancio print_string
+
+
+jal rand 			#GENERA la direzione
+#$t2 contiene la direzione
+#$s4 contiene il pointer
+#$s2 contiene l'offset iniziale (pointer all'inizio della stringa labirinto)
+#$t5 contiene l'offset temporaneo per i controlli
+beq $t2, $zero, nord 	#0:nord
+addi $t2, $t2, -1
+beq $t2, $zero, est 	#1:est
+addi $t2, $t2, -1
+beq $t2, $zero, sud 	#2:sud
+addi $t2, $t2, -1
+beq $t2, $zero, ovest 	#3:ovest
+j ExitSwitch
+
+nord:
+addi $t5, $s2, 20
+blt $s4, $t5, ExitSwitch	#controlla se può andare a nord
+addi $s4, $s4, -10
+jal dot
+addi $s4, $s4, -10
+jal dot
+j ExitSwitch
+
+est:
+addi $t5, $s2, 17
+beq $s4, $t5, ExitSwitch	#controlla se può andare a est
+addi $t5, $s2, 37
+beq $s4, $t5, ExitSwitch
+addi $t5, $s2, 57
+beq $s4, $t5, ExitSwitch
+addi $t5, $s2, 77
+beq $s4, $t5, ExitSwitch
+addi $s4, $s4, -1
+jal dot
+addi $s4, $s4, -1
+jal dot
+j ExitSwitch
+
+sud:
+addi $t5, $s2, 60
+bgt $s4, $t5, ExitSwitch	#controlla se può andare a sud
+addi $s4, $s4, 10
+jal dot
+addi $s4, $s4, 10
+jal dot
+j ExitSwitch
+
+ovest:
+addi $t5, $s2, 11
+beq $s4, $t5, ExitSwitch	#controlla se può andare a ovest
+addi $t5, $s2, 31
+beq $s4, $t5, ExitSwitch
+addi $t5, $s2, 51
+beq $s4, $t5, ExitSwitch
+addi $t5, $s2, 71
+beq $s4, $t5, ExitSwitch
+addi $s4, $s4, -1
+jal dot
+addi $s4, $s4, -1
+jal dot
+j ExitSwitch
+
+ExitSwitch:
+#stack reload
+lw $ra, 0($sp)
+addi $sp, $sp, 4
+jr $ra
