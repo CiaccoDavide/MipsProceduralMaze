31997837e313499b46b65b4c68ce2d6896b794b6
CiaccoDavide <davideciacco@CDMB.local>
2015-06-18 21:11:15 +0200

    indentation

--- generatorx.asm
+++ generatorx.asm
@@ -6,7 +6,7 @@
 
 #	$s5 contiene il contatore delle zone esplorate
 #	$s6 contiene il contatore delle posizioni da 2 a 0 {si dovrà saltare la direzione di provenienza}
-#	$s7 contiene la direzione di provenienza 
+#	$s7 contiene la direzione di provenienza
 
 .data
 
@@ -21,330 +21,330 @@ stringS: .asciiz "\nInserire il seed: "
 .text
 main:
 
-j reset #genera la stringa di default chiedendo di definire le dimensioni del labirinto
-afterReset:
+	j reset #genera la stringa di default chiedendo di definire le dimensioni del labirinto
+	afterReset:
 
-la $s2, labirinto 	#non serve caricare subito la default finchè non si vuole fare il reset
-					#$s2 invece mi serve per calcolare l'offset sulla stringa $s1 > $s2+11
-la $s1, labirinto 	
+	la $s2, labirinto 	#non serve caricare subito la default finchè non si vuole fare il reset
+						#$s2 invece mi serve per calcolare l'offset sulla stringa $s1 > $s2+11
+	la $s1, labirinto
 
-jal seed
+	jal seed
 
-# x,y in $t8,$t9
-la $t1, xy
-lb $t8, 0($t1) #larghezza
-add $a0, $zero, $t8
-jal rand 			#GENERA la X
-add $t3, $zero, $v0 #salva la x in $t3
-lb $t9, 1($t1) #altezza
-add $a0, $zero, $t9
-jal rand 			#GENERA la Y
-add $t4, $zero, $v0 #salva la y in $t4
+	# x,y in $t8,$t9
+	la $t1, xy
+	lb $t8, 0($t1) #larghezza
+	add $a0, $zero, $t8
+	jal rand 			#GENERA la X
+	add $t3, $zero, $v0 #salva la x in $t3
+	lb $t9, 1($t1) #altezza
+	add $a0, $zero, $t9
+	jal rand 			#GENERA la Y
+	add $t4, $zero, $v0 #salva la y in $t4
 
-#DETERMINO L'OFFSET SULLA STRINGA DEL LABIRINTO e lo salvo in $t2
-mul $t3, $t3, 2		#(2*x)
+	#DETERMINO L'OFFSET SULLA STRINGA DEL LABIRINTO e lo salvo in $t2
+	mul $t3, $t3, 2		#(2*x)
 
-mul $t5, $t8, 2		# 2*larghezza
-addi $t5, $t5, 2	#(2*larghezza + 2)
-mul $t5, $t5, 2		#(2*larghezza + 2)*2
+	mul $t5, $t8, 2		# 2*larghezza
+	addi $t5, $t5, 2	#(2*larghezza + 2)
+	mul $t5, $t5, 2		#(2*larghezza + 2)*2
 
-mul $t4, $t4, $t5	#((2*larghezza + 2)*2)*y
+	mul $t4, $t4, $t5	#((2*larghezza + 2)*2)*y
 
-add $t2, $t3, $t4	#(2*x)+((2*larghezza+2)*2)*y
-add $t2, $t2, $t8	#(2*x)+((2*larghezza+2)*2)*y+larghezza
-add $t2, $t2, $t8	#(2*x)+((2*larghezza+2)*2)*y+2*larghezza
-addi $t2, $t2, 3	#(2*x)+((2*larghezza+2)*2)*y+(2*larghezza+3)
+	add $t2, $t3, $t4	#(2*x)+((2*larghezza+2)*2)*y
+	add $t2, $t2, $t8	#(2*x)+((2*larghezza+2)*2)*y+larghezza
+	add $t2, $t2, $t8	#(2*x)+((2*larghezza+2)*2)*y+2*larghezza
+	addi $t2, $t2, 3	#(2*x)+((2*larghezza+2)*2)*y+(2*larghezza+3)
 
-#POSIZIONO IL PUNTO DI PARTENZA
-li $t0, 65			#carico il carattere A di partenza in $t0
-add $s1, $s1, $t2	#sposto il puntatore sulla casella
-sb $t0, ($s1)		#salva il carattere A nel labirinto
+	#POSIZIONO IL PUNTO DI PARTENZA
+	li $t0, 65			#carico il carattere A di partenza in $t0
+	add $s1, $s1, $t2	#sposto il puntatore sulla casella
+	sb $t0, ($s1)		#salva il carattere A nel labirinto
 
-addi $a2, $zero, 4 	#direzione di provenienza nulla
-addi $s3, $zero, 1	#setta 'zone esplorate' a uno
-#setta le direzioni provate tutte a 'no'
-addi $s4, $zero, 0	#direzione provata? nord [1:no,0:si,2:provenienza]
-addi $s5, $zero, 0	#direzione provata? est
-addi $s6, $zero, 0	#direzione provata? sud
-addi $s7, $zero, 0	#direzione provata? ovest
-#j termina
-j esplora #funz ricorsiva
+	addi $a2, $zero, 4 	#direzione di provenienza nulla
+	addi $s3, $zero, 1	#setta 'zone esplorate' a uno
+	#setta le direzioni provate tutte a 'no'
+	addi $s4, $zero, 0	#direzione provata? nord [1:no,0:si,2:provenienza]
+	addi $s5, $zero, 0	#direzione provata? est
+	addi $s6, $zero, 0	#direzione provata? sud
+	addi $s7, $zero, 0	#direzione provata? ovest
+	#j termina
+	j esplora #funz ricorsiva
 
 
 
 
 esplora:
 
-ricalcolaDirezione:
-#VerificaDirezione:
-#controlla se tutte le zone sono state esplorate ($s3==16)
-mul $t7, $t8, $t9	#numero di zone esplorabili (lo posso calcolare ogni volta visto che e' storato in dei registri temporanei, ma per ora e' uno spreco di risorse anche se non conforme alle convenzioni MIPS)
-beq $s3, $t7, termina #il termina stampera' direttamente la B
-#controlla se tutte le direzioni sono state provate
-beq $s4, $zero, continua
-beq $s5, $zero, continua
-beq $s6, $zero, continua
-beq $s7, $zero, continua
-j passoIndietro
-continua:
+	ricalcolaDirezione:
+	#VerificaDirezione:
+	#controlla se tutte le zone sono state esplorate ($s3==16)
+	mul $t7, $t8, $t9	#numero di zone esplorabili (lo posso calcolare ogni volta visto che e' storato in dei registri temporanei, ma per ora e' uno spreco di risorse anche se non conforme alle convenzioni MIPS)
+	beq $s3, $t7, termina #il termina stampera' direttamente la B
+	#controlla se tutte le direzioni sono state provate
+	beq $s4, $zero, continua
+	beq $s5, $zero, continua
+	beq $s6, $zero, continua
+	beq $s7, $zero, continua
+	j passoIndietro
+	continua:
 
-#$v0 contiene la direzione
-#$s1 contiene il pointer
-#$s2 contiene l'offset iniziale (pointer all'inizio della stringa labirinto)
-#$t1 e $t2 utilizzati per i controlli di spostamento
-#$s4,$s5,$s6,$s7 utilizzati per controllare di aver provato tutte le direzioni
+	#$v0 contiene la direzione
+	#$s1 contiene il pointer
+	#$s2 contiene l'offset iniziale (pointer all'inizio della stringa labirinto)
+	#$t1 e $t2 utilizzati per i controlli di spostamento
+	#$s4,$s5,$s6,$s7 utilizzati per controllare di aver provato tutte le direzioni
 
-#ricalcolaDirezione:
-addi $a0, $zero, 4		#set dell'argomento da passare a rand
-jal rand 				#GENERA la direzione e la controlla
-add $a2, $zero, $v0		#salva la direzione di movimento per passarla al passo avanti
-beq $v0, $zero, nord 	#0:nord
-addi $v0, $v0, -1
-beq $v0, $zero, est 	#1:est
-addi $v0, $v0, -1
-beq $v0, $zero, sud 	#2:sud
-addi $v0, $v0, -1
-beq $v0, $zero, ovest 	#3:ovest
-j ExitSwitch	#ATTENZIONE: qui abbiamo portato $t0 a 0, quindi non possiamo più ricavarne la dir di prov per il passo successivo!
+	#ricalcolaDirezione:
+	addi $a0, $zero, 4		#set dell'argomento da passare a rand
+	jal rand 				#GENERA la direzione e la controlla
+	add $a2, $zero, $v0		#salva la direzione di movimento per passarla al passo avanti
+	beq $v0, $zero, nord 	#0:nord
+	addi $v0, $v0, -1
+	beq $v0, $zero, est 	#1:est
+	addi $v0, $v0, -1
+	beq $v0, $zero, sud 	#2:sud
+	addi $v0, $v0, -1
+	beq $v0, $zero, ovest 	#3:ovest
+	j ExitSwitch	#ATTENZIONE: qui abbiamo portato $t0 a 0, quindi non possiamo più ricavarne la dir di prov per il passo successivo!
 
-nord:
-#controlla se la destinazione è già stata provata
-bne $s4, 0, ricalcolaDirezione
-#segna che ha considerato questa direzione
-addi $s4, $zero, 1
-#controlla se può andare a nord
-mul $t7, $t8, 2		# 2*larghezza
-addi $t7, $t7, 2	#(2*larghezza + 2)
-mul $t7, $t7, 3		#(2*larghezza + 2)*2
-add $t1, $s2, $t7
-blt $s1, $t1, ricalcolaDirezione
-#controlla se la destinazione è già stata esplorata anche da qualche altro punto
+	nord:
+	#controlla se la destinazione è già stata provata
+	bne $s4, 0, ricalcolaDirezione
+	#segna che ha considerato questa direzione
+	addi $s4, $zero, 1
+	#controlla se può andare a nord
+	mul $t7, $t8, 2		# 2*larghezza
+	addi $t7, $t7, 2	#(2*larghezza + 2)
+	mul $t7, $t7, 3		#(2*larghezza + 2)*2
+	add $t1, $s2, $t7
+	blt $s1, $t1, ricalcolaDirezione
+	#controlla se la destinazione è già stata esplorata anche da qualche altro punto
 
-mul $t7, $t8, 4
-addi $t7, $t7, 4
+	mul $t7, $t8, 4
+	addi $t7, $t7, 4
 
-sub $t1, $s1, $t7
-lb $t1, ($t1)
-bne $t1, 35, ricalcolaDirezione	#se è # allora non è ancora esplorato (carattere 35 = #)
+	sub $t1, $s1, $t7
+	lb $t1, ($t1)
+	bne $t1, 35, ricalcolaDirezione	#se è # allora non è ancora esplorato (carattere 35 = #)
 
-div $t7, $t7, 2
-sub $s1, $s1, $t7
-jal dot
-sub $s1, $s1, $t7
-jal dot
-j ExitSwitch
+	div $t7, $t7, 2
+	sub $s1, $s1, $t7
+	jal dot
+	sub $s1, $s1, $t7
+	jal dot
+	j ExitSwitch
 
-est:
-#controlla se la destinazione è già stata provata
-bne $s5, 0, ricalcolaDirezione
-#segna che ha considerato questa direzione
-addi $s5, $zero, 1
-#controlla se può andare a est
+	est:
+	#controlla se la destinazione è già stata provata
+	bne $s5, 0, ricalcolaDirezione
+	#segna che ha considerato questa direzione
+	addi $s5, $zero, 1
+	#controlla se può andare a est
 
-mul $t7, $t8, 2
-addi $t7, $t7, 3
-sub $t6, $s1, $s2 #$s1-$s2
-sub $t7, $t6, $t7 #($s1-$s2)-(2*larghezza+3)
-addi $t6, $t8, -1 #(x-1)
-mul $t6, $t6, 2  #2*(x-1)
-add $t7, $t7, $t6 #2*(x-1)+($s1-$s2)-(2*larghezza+3)
-beq $t7, $zero, ricalcolaDirezione
+	mul $t7, $t8, 2
+	addi $t7, $t7, 3
+	sub $t6, $s1, $s2 #$s1-$s2
+	sub $t7, $t6, $t7 #($s1-$s2)-(2*larghezza+3)
+	addi $t6, $t8, -1 #(x-1)
+	mul $t6, $t6, 2  #2*(x-1)
+	add $t7, $t7, $t6 #2*(x-1)+($s1-$s2)-(2*larghezza+3)
+	beq $t7, $zero, ricalcolaDirezione
 
-mul $t6, $t8, 2		# 2*larghezza
-addi $t6, $t6, 2	#(2*larghezza + 2)
-mul $t6, $t6, 2		#(2*larghezza + 2)*2
-div $t7, $t7, $t6
-mfhi $t7
-beq $t7, $zero, ricalcolaDirezione
+	mul $t6, $t8, 2		# 2*larghezza
+	addi $t6, $t6, 2	#(2*larghezza + 2)
+	mul $t6, $t6, 2		#(2*larghezza + 2)*2
+	div $t7, $t7, $t6
+	mfhi $t7
+	beq $t7, $zero, ricalcolaDirezione
 
-#controlla se la destinazione è già stata esplorata
-addi $t1, $s1, 2
-lb $t1, ($t1)
-bne $t1, 35, ricalcolaDirezione	#se è # allora non è ancora esplorato (carattere 35 = #)
+	#controlla se la destinazione è già stata esplorata
+	addi $t1, $s1, 2
+	lb $t1, ($t1)
+	bne $t1, 35, ricalcolaDirezione	#se è # allora non è ancora esplorato (carattere 35 = #)
 
-addi $s1, $s1, 1
-jal dot
-addi $s1, $s1, 1
-jal dot
-j ExitSwitch
+	addi $s1, $s1, 1
+	jal dot
+	addi $s1, $s1, 1
+	jal dot
+	j ExitSwitch
 
-sud:
-#controlla se la destinazione è già stata provata
-bne $s6, 0, ricalcolaDirezione
-#segna che ha considerato questa direzione
-addi $s6, $zero, 1
-#controlla se può andare a sud
+	sud:
+	#controlla se la destinazione è già stata provata
+	bne $s6, 0, ricalcolaDirezione
+	#segna che ha considerato questa direzione
+	addi $s6, $zero, 1
+	#controlla se può andare a sud
 
-mul $t7, $t8, 2
-addi $t7, $t7, 2
-mul $t6, $t9, 2
-addi $t6, $t6, 2
-mul $t7, $t7, $t6#lunghezza stringa
-mul $t6, $t8, 2#larghezza*2
-addi $t6, $t6, 2#larghezza*2)+2
-mul $t6, $t6, 3
-sub $t7, $t7, $t6
+	mul $t7, $t8, 2
+	addi $t7, $t7, 2
+	mul $t6, $t9, 2
+	addi $t6, $t6, 2
+	mul $t7, $t7, $t6#lunghezza stringa
+	mul $t6, $t8, 2#larghezza*2
+	addi $t6, $t6, 2#larghezza*2)+2
+	mul $t6, $t6, 3
+	sub $t7, $t7, $t6
 
-add $t1, $s2, $t7
-bgt $s1, $t1, ricalcolaDirezione
-#controlla se la destinazione è già stata esplorata
+	add $t1, $s2, $t7
+	bgt $s1, $t1, ricalcolaDirezione
+	#controlla se la destinazione è già stata esplorata
 
-#prima ricalcola la casella sotto
-mul $t7, $t8, 4
-addi $t7, $t7, 4
+	#prima ricalcola la casella sotto
+	mul $t7, $t8, 4
+	addi $t7, $t7, 4
 
-add $t1, $s1, $t7
-lb $t1, ($t1)
-bne $t1, 35, ricalcolaDirezione	#se è # allora non è ancora esplorato (carattere 35 = #)
+	add $t1, $s1, $t7
+	lb $t1, ($t1)
+	bne $t1, 35, ricalcolaDirezione	#se è # allora non è ancora esplorato (carattere 35 = #)
 
-div $t7, $t7, 2
-add $s1, $s1, $t7
-jal dot
-add $s1, $s1, $t7
-jal dot
-j ExitSwitch
+	div $t7, $t7, 2
+	add $s1, $s1, $t7
+	jal dot
+	add $s1, $s1, $t7
+	jal dot
+	j ExitSwitch
 
-ovest:
-#controlla se la destinazione è già stata provata
-bne $s7, 0, ricalcolaDirezione
-#segna che ha considerato questa direzione
-addi $s7, $zero, 1
-#controlla se può andare a ovest
+	ovest:
+	#controlla se la destinazione è già stata provata
+	bne $s7, 0, ricalcolaDirezione
+	#segna che ha considerato questa direzione
+	addi $s7, $zero, 1
+	#controlla se può andare a ovest
 
-mul $t7, $t8, 2
-addi $t7, $t7, 3
-sub $t6, $s1, $s2 #$s1-$s2
-sub $t7, $t6, $t7 #($s1-$s2)-(2*larghezza+3)
-beq $t7, $zero, ricalcolaDirezione
+	mul $t7, $t8, 2
+	addi $t7, $t7, 3
+	sub $t6, $s1, $s2 #$s1-$s2
+	sub $t7, $t6, $t7 #($s1-$s2)-(2*larghezza+3)
+	beq $t7, $zero, ricalcolaDirezione
 
-mul $t6, $t8, 2		# 2*larghezza
-addi $t6, $t6, 2	#(2*larghezza + 2)
-mul $t6, $t6, 2		#(2*larghezza + 2)*2
-div $t7, $t7, $t6
-mfhi $t7
-beq $t7, $zero, ricalcolaDirezione
-
-
-#controlla se la destinazione è già stata esplorata
-addi $t1, $s1, -2
-lb $t1, ($t1)
-bne $t1, 35, ricalcolaDirezione	#se è # allora non è ancora esplorato (carattere 35 = #)
-
-addi $s1, $s1, -1
-jal dot
-addi $s1, $s1, -1
-jal dot
-j ExitSwitch
-
-ExitSwitch:
-
-#stack save
-addi $sp, $sp, -20
-#sw $fp, 20($sp)
-sw $s7, 16($sp)
-sw $s6, 12($sp)
-sw $s5, 8($sp)
-sw $s4, 4($sp)
-sw $ra, 0($sp) #provenienza
-addi $s3, $s3, 1 #aggiungi 1 alle zone esplorate (solo quando si muove in avanti)
+	mul $t6, $t8, 2		# 2*larghezza
+	addi $t6, $t6, 2	#(2*larghezza + 2)
+	mul $t6, $t6, 2		#(2*larghezza + 2)*2
+	div $t7, $t7, $t6
+	mfhi $t7
+	beq $t7, $zero, ricalcolaDirezione
 
 
-#setta la provenienza (invertita perche' dovra' essere usata dalla prossima posizione)
-add $t0, $zero, $a2
-bne $t0, $zero, notNord
-addi $s4, $zero, 0
-addi $s5, $zero, 0
-addi $s6, $zero, 2
-addi $s7, $zero, 0
-j notOvest
-notNord:
-addi $t0, $t0, -1
-bne $t0, $zero, notEst
-addi $s4, $zero, 0
-addi $s5, $zero, 0
-addi $s6, $zero, 0
-addi $s7, $zero, 2
-j notOvest
-notEst:
-addi $t0, $t0, -1
-bne $t0, $zero, notSud
-addi $s4, $zero, 2
-addi $s5, $zero, 0
-addi $s6, $zero, 0
-addi $s7, $zero, 0
-j notOvest
-notSud:
-addi $t0, $t0, -1
-bne $t0, $zero, notOvest
-addi $s4, $zero, 0
-addi $s5, $zero, 2
-addi $s6, $zero, 0
-addi $s7, $zero, 0
-notOvest:
+	#controlla se la destinazione è già stata esplorata
+	addi $t1, $s1, -2
+	lb $t1, ($t1)
+	bne $t1, 35, ricalcolaDirezione	#se è # allora non è ancora esplorato (carattere 35 = #)
 
-j esplora
+	addi $s1, $s1, -1
+	jal dot
+	addi $s1, $s1, -1
+	jal dot
+	j ExitSwitch
+
+	ExitSwitch:
+
+	#stack save
+	addi $sp, $sp, -20
+	#sw $fp, 20($sp)
+	sw $s7, 16($sp)
+	sw $s6, 12($sp)
+	sw $s5, 8($sp)
+	sw $s4, 4($sp)
+	sw $ra, 0($sp) #provenienza
+	addi $s3, $s3, 1 #aggiungi 1 alle zone esplorate (solo quando si muove in avanti)
 
 
+	#setta la provenienza (invertita perche' dovra' essere usata dalla prossima posizione)
+	add $t0, $zero, $a2
+	bne $t0, $zero, notNord
+	addi $s4, $zero, 0
+	addi $s5, $zero, 0
+	addi $s6, $zero, 2
+	addi $s7, $zero, 0
+	j notOvest
+	notNord:
+	addi $t0, $t0, -1
+	bne $t0, $zero, notEst
+	addi $s4, $zero, 0
+	addi $s5, $zero, 0
+	addi $s6, $zero, 0
+	addi $s7, $zero, 2
+	j notOvest
+	notEst:
+	addi $t0, $t0, -1
+	bne $t0, $zero, notSud
+	addi $s4, $zero, 2
+	addi $s5, $zero, 0
+	addi $s6, $zero, 0
+	addi $s7, $zero, 0
+	j notOvest
+	notSud:
+	addi $t0, $t0, -1
+	bne $t0, $zero, notOvest
+	addi $s4, $zero, 0
+	addi $s5, $zero, 2
+	addi $s6, $zero, 0
+	addi $s7, $zero, 0
+	notOvest:
+
+	j esplora
 
 
 
 
-passoIndietro:
-#fa un passo indietro nella direzione da cui era arrivato (0:sud,1:ovest,2:nord,3:est)
-beq $s4, 2, backToNord
-beq $s5, 2, backToEst
-beq $s6, 2, backToSud
-beq $s7, 2, backToOvest
 
-#j wentBack
-j termina
 
-backToNord:
+	passoIndietro:
+	#fa un passo indietro nella direzione da cui era arrivato (0:sud,1:ovest,2:nord,3:est)
+	beq $s4, 2, backToNord
+	beq $s5, 2, backToEst
+	beq $s6, 2, backToSud
+	beq $s7, 2, backToOvest
 
-mul $t7, $t8, 2		# 2*larghezza
-addi $t7, $t7, 2	#(2*larghezza + 2)
+	#j wentBack
+	j termina
 
-jal pulisci
-sub $s1, $s1, $t7
-jal pulisci
-sub $s1, $s1, $t7
-j wentBack
+	backToNord:
 
-backToEst:
-jal pulisci
-addi $s1, $s1, 1
-jal pulisci
-addi $s1, $s1, 1
-j wentBack
+	mul $t7, $t8, 2		# 2*larghezza
+	addi $t7, $t7, 2	#(2*larghezza + 2)
 
-backToSud:
+	jal pulisci
+	sub $s1, $s1, $t7
+	jal pulisci
+	sub $s1, $s1, $t7
+	j wentBack
 
-mul $t7, $t8, 2		# 2*larghezza
-addi $t7, $t7, 2	#(2*larghezza + 2)
+	backToEst:
+	jal pulisci
+	addi $s1, $s1, 1
+	jal pulisci
+	addi $s1, $s1, 1
+	j wentBack
 
-jal pulisci
-add $s1, $s1, $t7
-jal pulisci
-add $s1, $s1, $t7
-j wentBack
+	backToSud:
 
-backToOvest:
-jal pulisci
-addi $s1, $s1, -1
-jal pulisci
-addi $s1, $s1, -1
-j wentBack
+	mul $t7, $t8, 2		# 2*larghezza
+	addi $t7, $t7, 2	#(2*larghezza + 2)
 
-wentBack:
-#stack reload
-#lw $fp, 20($sp)
-lw $s7, 16($sp)
-lw $s6, 12($sp)
-lw $s5, 8($sp)
-lw $s4, 4($sp)
-lw $ra, 0($sp) #provenienza
-addi $sp, $sp, 20
-j esplora
+	jal pulisci
+	add $s1, $s1, $t7
+	jal pulisci
+	add $s1, $s1, $t7
+	j wentBack
+
+	backToOvest:
+	jal pulisci
+	addi $s1, $s1, -1
+	jal pulisci
+	addi $s1, $s1, -1
+	j wentBack
+
+	wentBack:
+	#stack reload
+	#lw $fp, 20($sp)
+	lw $s7, 16($sp)
+	lw $s6, 12($sp)
+	lw $s5, 8($sp)
+	lw $s4, 4($sp)
+	lw $ra, 0($sp) #provenienza
+	addi $sp, $sp, 20
+	j esplora
 
 
 
@@ -431,7 +431,7 @@ reset:
 	div $t0, $v0, 2
 	mfhi $t0
 	bne $t0, 0, reset 		#x%2==0
-	
+
 	la $t1, xy
 	sb $v0, ($t1)
 
@@ -447,7 +447,7 @@ reset:
 	div $t0, $v0, 2
 	mfhi $t0
 	bne $t0, 0, reset 		#x%2==0
-	
+
 	sb $v0, 1($t1)
 
 	la $s2, labirinto 		#carica l'indirizzo di labirinto in $s2
@@ -462,7 +462,7 @@ reset:
 		lb $t2, ($t1)			#carico la x in t1
 		mul $t2, $t2, 2
 		addi $t2, $t2, 1
-	
+
 	forx:
 		addi $t0, $zero, 35
 		sb $t0, ($s2)
@@ -489,7 +489,7 @@ j afterReset
 
 
 
-seed:	
+seed:
 	li $v0, 4				# selezione di print_string (codice = 4)
 	la $a0, stringS			# $a0 = indirizzo di string1
 	syscall					# lancio print_string
@@ -513,13 +513,3 @@ rand:	#restituisce in $t0 un valore pseudorandom [0..$a0]
 	mfhi $t0
 	abs $v0 $t0  #l'abs potrebbe essere semplice usando una xor?
 	jr $ra
-
-
-
-
-
-
-
-
-
-
